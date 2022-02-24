d6198b5d290dced4b7c917bd2938b648a67b5035
Vinicius <vinioliveira.s@hotmail.com>
2022-02-24 00:00:23 -0300

    Deleting unnecessary files

--- compexec.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-javac src/*.java
-javac mains/SingleThread.java
-java mains.SingleThread
--- cpe.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-javac src/*.java
-javac mains/MultiThread.java
-java mains.MultiThread
