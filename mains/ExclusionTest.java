e8ea0feafbb6dcb9d66bc28567178da5dfd078c1
Vinicius <vinioliveira.s@hotmail.com>
2022-02-23 23:59:22 -0300

    Adding a main file for each test case

---
+++ mains/ExclusionTest.java
@@ -0,0 +1,26 @@
+package mains;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+import src.*;
+
+
+public class ExclusionTest {
+    public static void main(String[] args) throws InterruptedException{
+        RentManager manager = new RentManager();
+
+        manager.addClient(new Client(0));
+        manager.addClient(new Client(1));
+
+        manager.addCar(new Car(0));
+
+        Semaphore sem = new Semaphore(1);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(new RentThread(manager, manager.getCars().get(0), manager.getClients().get(0), sem));
+        executor.execute(new RentThread(manager, manager.getCars().get(0), manager.getClients().get(1), sem));
+        executor.shutdown();   
+    }
+    
+}
---
+++ mains/SingleThreadPerformance.java
@@ -0,0 +1,35 @@
+package mains;
+
+import src.*;
+
+public class SingleThread {
+    public static void main(String[] args) {
+
+        long begin = System.currentTimeMillis();
+
+        RentManager manager = new RentManager();
+
+        int clients = 200;
+        int cars = 50;
+
+        for (int i = 0 ; i < cars; i++) {
+            manager.addCar(new Car(i));
+        }
+        for (int i = 0; i < clients; i++) {
+            manager.addClient(new Client(i));
+        }
+        for (int i = 0; i < clients; i++) {
+            boolean aux;
+            aux = manager.rentCar(manager.getCars().get(i%cars), manager.getClients().get(i));
+            if(aux)
+                System.out.println(i+" Rent executed!");   
+            else
+                System.out.println(i+" Car already rented!");
+        }
+        
+       
+    
+        long end = System.currentTimeMillis();
+        System.out.println("Levou "+(end-begin)+" milisegundos");
+    }
+}
